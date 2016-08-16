a3e493a1767afe533c54d063410d457b4d75ef78
lynnding <lynnding@china-liantong.com>
2016-08-16 21:38:17 +0800

    add bug deadline time

--- redminefreedom.py
+++ redminefreedom.py
@@ -7,17 +7,16 @@ import requests
 import os
 import re
 import logging
+import argparse
 from bs4 import BeautifulSoup, NavigableString
 import time
 import datetime
 
 REDMINE_URL='http://redmine.china-liantong.com:8000'
 
-
-#head = {
-#    "user-agent":"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.87 Safari/537.36",
-#}
-
+header = {
+        "user-agent":"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.87 Safari/537.36",
+}
 
 def setup_logger():
     logger = logging.getLogger()
@@ -45,9 +44,6 @@ def user_login(user, pwd):
         'password' : pwd,
         'username' : user,
     }
-    header = {
-            "user-agent":"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.87 Safari/537.36",
-    }
     r = s.post(login_url, headers=header, data=post_data)
     if r.status_code != requests.codes.ok:
         logging.error('POST:>>'+login_url+'<< error:' + str(r.status_code))
@@ -58,9 +54,6 @@ def user_login(user, pwd):
 
 
 def request_page(session, url, payload={}):
-    header = {
-        "user-agent":"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.87 Safari/537.36",
-    }
     r = session.get(url, params=payload, headers=header)
     if r.status_code != requests.codes.ok:
         logging.error('GET:>>'+ url +'<< error:' + str(r.status_code))
@@ -117,69 +110,79 @@ def get_issues(session):
             bug_id_tag = child.find_all('td', 'id')
             if bug_id_tag and bug_id_tag[0].a:
                 result['bugid'] = bug_id_tag[0].a.contents[0]
-                logging.info(result['bugid'])
+                #logging.info(result['bugid'])
 
             project_tag = child.find_all('td', 'project')
             if project_tag and project_tag[0].a:
                 result['project'] = project_tag[0].a.contents[0]
-                logging.info(result['project'])
+                #logging.info(result['project'])
 
             attr_tag = child.find_all('td', 'tracker')
             if attr_tag and attr_tag[0].contents:
                 result['attr'] = attr_tag[0].contents[0]
-                logging.info(result['attr'])
+                #logging.info(result['attr'])
 
             status_tag = child.find_all('td', 'status')
             if status_tag and status_tag[0].contents:
                 result['status'] = status_tag[0].contents[0]
-                logging.info(result['status'])
+                #logging.info(result['status'])
 
             priority_tag = child.find_all('td', 'priority')
             if priority_tag and priority_tag[0].contents:
                 result['priority'] = priority_tag[0].contents[0]
-                logging.info(result['priority'])
+                #logging.info(result['priority'])
 
             title_tag = child.find_all('td', 'subject')
             if title_tag and title_tag[0].a:
                 result['title'] = title_tag[0].a.contents[0]
-                logging.info(result['title'])
+                #logging.info(result['title'])
 
             owner_tag = child.find_all('td', 'assigned_to')
             if owner_tag and owner_tag[0].a:
                 result['owner'] = owner_tag[0].a.contents[0]
-                logging.info(result['owner'])
+                #logging.info(result['owner'])
 
             start_date = child.find_all('td', 'start_date')
             if start_date and start_date[0].contents:
                 result['start'] = start_date[0].contents[0]
-                logging.info(result['start'])
+                #logging.info(result['start'])
 
             due_date = child.find_all('td', 'due_date')
             if due_date and due_date[0].contents:
                 result['end'] = due_date[0].contents[0]
-                logging.info(result['end'])
+                #logging.info(result['end'])
 
             progress = child.find_all('td', 'done_ratio')
             if progress and progress[0].table:
                 result['progress'] = progress[0].table['class'][1][len('progress-'):]
-                logging.info(result['progress'])
+                #logging.info(result['progress'])
 
             severity = child.find_all('td', 'cf_3')
             if severity and severity[0].contents:
                 result['severity'] = severity[0].contents[0]
-                logging.info(result['severity'])
+                #logging.info(result['severity'])
 
             bug_list.append(result)
 
     return bug_list
 
+
+def today(ftime = None):
+    now = datetime.datetime.now()
+    fnow = now.strftime('%Y-%m-%d')
+    if ftime is None:
+        return fnow
+    else:
+        delta =  datetime.datetime.strptime(ftime, '%Y-%m-%d') - datetime.datetime.strptime(fnow, '%Y-%m-%d')
+        return delta.days
+
 def main():
     setup_logger()
-    #parser = argparse.ArgumentParser()
-    #parser.add_argument("-u", "--user", help="Username", required=True)
-    #parser.add_argument("-u", "--password", help="Password", required=True)
-    #args = parser.parse_args()
-    session = user_login('lynnding', 'Wasd1234')
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-u", "--user", help="Username", required=True)
+    parser.add_argument("-p", "--password", help="Password", required=True)
+    args = parser.parse_args()
+    session = user_login(args.user, args.password)
 
     hours = get_last_7_hours(session)
     for key in hours:
@@ -187,16 +190,18 @@ def main():
             logging.error('work {0} hours in {1} less than 8 hours'.format(hours[key], key))
 
     bugs = get_issues(session)
-    logging.info(bugs)
+    for bug in bugs:
+        if bug.has_key('end'):
+            delta = today(bug['end'])
+            if delta <= 0:
+                logging.info('bug {0} is end at {1}, we need to delay'.format(bug['bugid'], bug['end']))
+        else:
+            logging.info('bug {0} have no end time, ignore'.format(bug['bugid']))
+    #logging.info(bugs)
 
 
 
 
 if __name__ == '__main__':
     main()
-
-
-
-
-
 
