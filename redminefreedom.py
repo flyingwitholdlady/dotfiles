d09367fb25d111eea14b15ab53c705c3febc18ba
lynnding <lynnding@china-liantong.com>
2016-08-19 15:41:37 +0800

    add date params

--- redminefreedom.py
+++ redminefreedom.py
@@ -6,6 +6,7 @@
 import requests
 from requests.auth import HTTPBasicAuth
 import os
+import sys
 import re
 import json
 import logging
@@ -35,7 +36,7 @@ def user_login(user, pwd):
     s = requests.session();
     r = request_page(s, login_url)
     if r == None:
-        os.exit(0)
+        sys.exit()
 
     token = re.findall('<input name="authenticity_token" type="hidden" value="(.*?)" />', r.content, re.S)[0]
     post_data =  {
@@ -84,91 +85,6 @@ def request_page(session, url, payload = None, header = None, auth = None):
 #    return result;
 #
 #
-#def get_issues(session):
-#    bug_list=[]
-#    issue_page = REDMINE_URL + '/issues'
-#    payload = { 'c[]':[ 'project', 'tracker', 'parent', 'status', 'priority',
-#                        'subject', 'assigned_to', 'start_date', 'due_date', 'done_ratio',
-#                        'cf_3'],
-#                'f[]':['status_id', 'tracker_id','assigned_to_id', ''],
-#                'group_by':'',
-#                'op[assigned_to_id]' :'=',
-#                'op[status_id]' :'=',
-#                'op[tracker_id]' :'!',
-#                'set_filter':1,
-#                'utf8':'E2%9C%93',
-#                'v[assigned_to_id][]':'me',
-#                'v[status_id][]':[34, 8, 23, 17, 31, 33],
-#                'v[tracker_id][]':[5],
-#            }
-#    r = request_page(session, issue_page, payload)
-#    if r == None:
-#        os.exit(0)
-#
-#    soup = BeautifulSoup(r.content, 'html.parser')
-#    list_tasks = soup.find_all('tbody')
-#    for child in list_tasks[0].children:
-#        if not isinstance(child, NavigableString):
-#            result={}
-#            bug_id_tag = child.find_all('td', 'id')
-#            if bug_id_tag and bug_id_tag[0].a:
-#                result['bugid'] = bug_id_tag[0].a.contents[0]
-#                #logging.info(result['bugid'])
-#
-#            project_tag = child.find_all('td', 'project')
-#            if project_tag and project_tag[0].a:
-#                result['project'] = project_tag[0].a.contents[0]
-#                #logging.info(result['project'])
-#
-#            attr_tag = child.find_all('td', 'tracker')
-#            if attr_tag and attr_tag[0].contents:
-#                result['attr'] = attr_tag[0].contents[0]
-#                logging.info(result['attr'])
-#
-#            status_tag = child.find_all('td', 'status')
-#            if status_tag and status_tag[0].contents:
-#                result['status'] = status_tag[0].contents[0]
-#                #logging.info(result['status'])
-#
-#            priority_tag = child.find_all('td', 'priority')
-#            if priority_tag and priority_tag[0].contents:
-#                result['priority'] = priority_tag[0].contents[0]
-#                #logging.info(result['priority'])
-#
-#            title_tag = child.find_all('td', 'subject')
-#            if title_tag and title_tag[0].a:
-#                result['title'] = title_tag[0].a.contents[0]
-#                #logging.info(result['title'])
-#
-#            owner_tag = child.find_all('td', 'assigned_to')
-#            if owner_tag and owner_tag[0].a:
-#                result['owner'] = owner_tag[0].a.contents[0]
-#                #logging.info(result['owner'])
-#
-#            start_date = child.find_all('td', 'start_date')
-#            if start_date and start_date[0].contents:
-#                result['start'] = start_date[0].contents[0]
-#                #logging.info(result['start'])
-#
-#            due_date = child.find_all('td', 'due_date')
-#            if due_date and due_date[0].contents:
-#                result['end'] = due_date[0].contents[0]
-#                #logging.info(result['end'])
-#
-#            progress = child.find_all('td', 'done_ratio')
-#            if progress and progress[0].table:
-#                result['progress'] = progress[0].table['class'][1][len('progress-'):]
-#                #logging.info(result['progress'])
-#
-#            severity = child.find_all('td', 'cf_3')
-#            if severity and severity[0].contents:
-#                result['severity'] = severity[0].contents[0]
-#                #logging.info(result['severity'])
-#
-#            bug_list.append(result)
-#
-#    return bug_list
-
 
 def today(ftime = None):
     now = datetime.datetime.now()
@@ -186,7 +102,7 @@ def get_operation_list(session):
     payload = { 'assigned_to_id': 'me', 'set_filter':1, 'priority':'desc,updated_on:desc'}
     r = request_page(session, issue_page, payload)
     if r == None:
-        os.exit(0)
+        sys.exit()
     result = re.search('availableFilters = {.*?};', r.content)
     if result == None:
         logging.info('can\'t find availableFilters var')
@@ -248,12 +164,11 @@ def get_issues(session, auth = None, filter_dict=None):
         json_addr += '&'
 
     json_addr = json_addr[:-1]
-    logging.info(json_addr)
     #json_addr='/issues.json?assigned_to_id=me&tracker_id=!5&status_id=8|31'
     r = request_page(session, REDMINE_URL + json_addr, header=header, auth=auth)
-    #logging.info(r.content)
+    logging.info(REDMINE_URL + json_addr)
     if r is None:
-        os.exit(0)
+        sys.exit()
     return dict(json.loads(r.content))
 
 
@@ -261,13 +176,13 @@ def get_issues(session, auth = None, filter_dict=None):
 def show_issues_state(issues = []):
     for  issue in issues:
         out=''
-        if issue['id']:
+        if 'id' in issue.keys():
             out += str(issue['id']).center(7)
             out += '||'
-        if issue['project']:
+        if 'project' in issue.keys():
             out += issue['project']['name'].ljust(30)
             out += '||'
-        if issue['tracker']:
+        if 'tracker' in issue.keys():
             out += issue['tracker']['name'].center(10)
             out += '||'
         if issue['status']:
@@ -295,6 +210,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-u", "--user", help="Username", required=True)
     parser.add_argument("-p", "--password", help="Password", required=True)
+    parser.add_argument("-d", "--delay", help="Delay", required=False, default='auto')
     args = parser.parse_args()
     session , token = user_login(args.user, args.password)
     auth = auth=HTTPBasicAuth(args.user, args.password)
@@ -304,15 +220,39 @@ def main():
                 }
     issues_dict = get_issues(session, auth, issue_filter)
     issues=[]
-    if issues_dict.has_key('issues'):
+    if 'issues' in issues_dict.keys():
         issues = issues_dict['issues'];
+    else:
+        logging.info('can\'t get redmine issue!')
+        sys.exit()
+
+    end_day = ''
+    if args.delay == 'auto':
+        end_day = (datetime.date.today() + datetime.timedelta(days=1)).strftime('%Y-%m-%d')
+        ## delay tomorrow
+    else:
+        try:
+            time.strptime(str(args.delay), "%Y-%m-%d")
+            end_day = args.delay
+        except:
+            logging.error('{0} is not a valid date'.format(args.delay))
+            return sys.exit()
+
+    logging.info('delay to {0}'.format(end_day))
+
     show_issues_state(issues)
-        #issues = list(issues_dict['issues'])
+    for issue in issues:
+        if issue.has_key('done_ratio') and int(issue['done_ratio']) == 100:
+            logging.info('{0}  progress is 100, ignore'.format(issue['id']))
+            continue
+        if not issue.has_key('due_date'):
+            logging.info('{0} have no end time, ignore'.format(issue['id']))
+            continue
+        delta = today(issue['due_date'])
+        if delta <= 0:
+            ##TODO
+            logging.info('issue {0} is end at {1}, we need to delay'.format(issue['id'], issue['due_date']))
     #logging.info(issues)
-    #users = get_operation_list(session)
-    #delay_test(session, token)
-
-
 
 
     #hours = get_last_7_hours(session)
